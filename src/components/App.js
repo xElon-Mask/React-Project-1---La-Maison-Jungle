4f11c24192cf4da39f99e2462877a2cda210fb7b
xElon-Mask <xelonmask@protonmail.com>
2022-05-04 15:09:30 +0200

    add Cart composant + clean src folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import Banner from "./Banner";
+import Cart from "./Cart";
 
 function App() {
-  return <Banner />;
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
 }
 
 export default App;
