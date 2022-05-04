aabe087d71d51ae3216523ad4f1e30b8dfcf2d5f
xElon-Mask <xelonmask@protonmail.com>
2022-05-04 15:40:19 +0200

    add logo and stylize for the banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,12 @@
 import "../styles/Banner.css";
+import logo from "../assets/logo.png";
 
 function Banner() {
+  const title = "La maison jungle";
   return (
     <div className="lmj-banner">
-      <h1>La Maison Jungle</h1>
+      <img src={logo} alt="La maison jungle" className="lmj-logo" />
+      <h1 className="lmj-title">{title}</h1>
     </div>
   );
 }
