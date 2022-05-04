b20b26a17f9ef65098c617aad285d7a77eed0ded
xElon-Mask <xelonmask@protonmail.com>
2022-05-04 15:26:25 +0200

    stylize Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,11 @@
+import "../styles/Banner.css";
+
 function Banner() {
-  return <h1>La Maison Jungle</h1>;
+  return (
+    <div className="lmj-banner">
+      <h1>La Maison Jungle</h1>
+    </div>
+  );
 }
 
 export default Banner;
